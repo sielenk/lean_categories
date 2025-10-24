de753302a5de862ac1ebfe340d6f8268f485068c
Marvin Sielenkemper <sielenk@gmx.de>
2025-10-24 08:34:04 +0200

    Add initial definitions for terminal and initial objects in Set category

--- Primus/Category.lean
+++ Primus/Category.lean
@@ -10,6 +10,7 @@ structure category.{m, n}: Sort _ where
 
 attribute [simp] category.left_id category.right_id
 
+
 structure initialObject(CC: category): Sort _ where
   I: CC.Ob
   hom(X: CC.Ob): CC.Hom I X
@@ -19,3 +20,53 @@ structure terminalObject(CC: category): Sort _ where
   T: CC.Ob
   hom(X: CC.Ob): CC.Hom X T
   unique{X: CC.Ob}(g: CC.Hom X T): hom X = g
+
+def isomorpic{CC: category}(A B: CC.Ob): Prop :=
+  ∃(f: CC.Hom A B)(g: CC.Hom B A), CC.compose g f = CC.id A ∧ CC.compose f g = CC.id B
+
+
+def mono{CC: category}{A B: CC.Ob}(f: CC.Hom A B): Prop :=
+  ∀{X: CC.Ob}(g1 g2: CC.Hom X A), CC.compose f g1 = CC.compose f g2 → g1 = g2
+
+def epi{CC: category}{A B: CC.Ob}(f: CC.Hom A B): Prop :=
+  ∀{X: CC.Ob}(g1 g2: CC.Hom B X), CC.compose g1 f = CC.compose g2 f → g1 = g2
+
+def splitMono{CC: category}{A B: CC.Ob}(f: CC.Hom A B): Prop :=
+  ∃(g: CC.Hom B A), CC.compose g f = CC.id A
+
+def splitEpi{CC: category}{A B: CC.Ob}(f: CC.Hom A B): Prop :=
+  ∃(g: CC.Hom B A), CC.compose f g = CC.id B
+
+def iso{CC: category}{A B: CC.Ob}(f: CC.Hom A B): Prop :=
+  ∃(g: CC.Hom B A), CC.compose g f = CC.id A ∧ CC.compose f g = CC.id B
+
+
+theorem splitMono_is_mono{CC: category}{A B: CC.Ob}(f: CC.Hom A B):
+  splitMono f → mono f := by
+  intro ⟨g, H1⟩ X g1 g2 H2
+  rw [←CC.left_id g1, ←CC.left_id g2, ←H1, ←CC.assoc, ←CC.assoc, H2]
+
+theorem splitEpi_is_epi{CC: category}{A B: CC.Ob}(f: CC.Hom A B):
+  splitEpi f → epi f := by
+  intro ⟨g, H1⟩ X g1 g2 H2
+  rw [←CC.right_id g1, ←CC.right_id g2, ←H1, CC.assoc, CC.assoc, H2]
+
+theorem splitMono_epi_is_iso{CC: category}{A B: CC.Ob}(f: CC.Hom A B):
+  splitMono f ∧ epi f → iso f := by
+  intro ⟨⟨g, H1⟩, H2⟩
+  apply Exists.intro g
+  and_intros
+  · assumption
+  . apply H2
+    rw [←CC.assoc, H1]
+    simp
+
+theorem splitEpi_mono_is_iso{CC: category}{A B: CC.Ob}(f: CC.Hom A B):
+  splitEpi f ∧ mono f → iso f := by
+  intro ⟨⟨g, H1⟩, H2⟩
+  apply Exists.intro g
+  and_intros
+  . apply H2
+    rw [CC.assoc, H1]
+    simp
+  . assumption
--- Primus/Set.lean
+++ Primus/Set.lean
@@ -10,3 +10,35 @@ def SetCat.{m}: category.{m+1, m} := {
   right_id _ := rfl
   assoc _ _ _ := rfl
 }
+
+def unitTerminal: terminalObject SetCat := {
+  T := PUnit
+  hom X := fun _ => PUnit.unit
+  unique g := by
+    funext x
+    cases g x
+    rfl
+}
+
+def emptyInitial: initialObject SetCat := {
+  I := PEmpty
+  hom X := fun e => PEmpty.elim e
+  unique g := by
+    funext x
+    cases x
+}
+
+theorem mono_injective{A B: SetCat.Ob}(f: SetCat.Hom A B):
+  mono f ↔ Function.Injective f := by
+  constructor
+  · intro H1 x1 x2 H2
+    have H3: ∀(X: SetCat.Ob)(x: A), SetCat.compose f (fun _: X => x) = fun _ => f x := by
+      intro X x
+      rfl
+    exact Hf
+  · intro H1 X g1 g2 H2
+    funext x
+    apply H1
+    have H3: (SetCat.compose f g1) x = f (g1 x) := by rfl
+    rw [←H3, H2]
+    rfl
