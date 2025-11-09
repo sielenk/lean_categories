1f6daaf3f646d04354fb2c00dc5ff2358c00a258
Marvin Sielenkemper <sielenk@gmx.de>
2025-11-09 17:50:17 +0100

    Refactor sort_epi_surjective theorem for clarity and conciseness; improve handling of cases and simplify expressions

--- Primus/SortCat.lean
+++ Primus/SortCat.lean
@@ -64,42 +64,46 @@ theorem sort_epi_surjective{A B: SortCat.{m+1}.Ob}(f: SortCat.Hom A B):
   epi f ↔ Function.Surjective f :=
 by
   constructor
-  · intro H1
-    false_or_by_contra
-    rename_i H2
-    unfold Function.Surjective at H2
-    rw [Classical.not_forall] at H2
-    replace ⟨b, H2⟩ := H2
-    replace H2: ∀ a, ¬f a = b := by
-      intro a H3
-      apply H2
-      exists a
+  · contrapose
+    intro H1
+    unfold Function.Surjective at H1
+    rw [Classical.not_forall] at H1
+    replace ⟨b, H1⟩ := H1
+    replace H1: ∀ a, ¬f a = b := by
+      intro a H2
+      apply H1
+      use a
     let g1: SortCat.Hom B twoOb := fun b' => twoOb.ob1
     let g2: SortCat.Hom B twoOb := fun b' =>
       match Classical.propDecidable (b' = b) with
       | isTrue _  => twoOb.ob2
       | isFalse _ => twoOb.ob1
+    intro H2
     have H3: g1 b = g2 b := by
-      rw  [H1 g1 g2]
+      rw  [H2 g1 g2]
       funext a
-      unfold SortCat g1 g2; simp
-      cases Classical.propDecidable (f a = b)
-      . rfl
-      . exfalso
-        apply H2 a
-        assumption
+      simp [SortCat]
+      unfold g1 g2
+      cases Classical.propDecidable (f a = b) with
+      | isFalse H =>
+        rfl
+      | isTrue H =>
+        exfalso
+        exact H1 a H
     unfold g1 g2 at H3
     revert H3
-    cases Classical.propDecidable (b = b)
-    . contradiction
-    . simp
+    cases Classical.propDecidable (b = b) with
+    | isFalse H =>
+      contradiction
+    | isTrue H =>
+      simp
   . intro H1 C g1 g2 H2
     funext b
     have ⟨a, H3⟩ := H1 b
     rw [←H3]
     have H4: (SortCat.compose g1 f) a = (SortCat.compose g2 f) a := by
       rw [H2]
-    unfold SortCat at H4; simp at H4
+    simp [SortCat] at H4
     assumption
 
 theorem sort_splitEpi_surjective{A B: SortCat.Ob}(f: SortCat.Hom A B):
@@ -107,15 +111,14 @@ theorem sort_splitEpi_surjective{A B: SortCat.Ob}(f: SortCat.Hom A B):
 by
   constructor
   · intro ⟨g, H1⟩ b
-    exists (g b)
+    use (g b)
     have H2: f (g b) = (SortCat.compose f g) b := by rfl
     rw [H2, H1]
     rfl
   . intro H1
-    exists (fun b => Classical.choose (H1 b))
+    use (fun b => Classical.choose (H1 b))
     funext b
-    unfold SortCat
-    simp
+    unfold SortCat; simp
     exact Classical.choose_spec (H1 b)
 
 theorem sort_splitEpi_epi.{m}{A B: SortCat.{m+1}.Ob}(f: SortCat.Hom A B):
