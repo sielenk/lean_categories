94dff20a2da37c3ac513e26240c0d61d3086ad7e
Marvin Sielenkemper <sielenk@gmx.de>
2025-11-23 15:13:33 +0100

    added the Yoneda embedding

--- Primus/Hom.lean
+++ Primus/Hom.lean
@@ -48,3 +48,53 @@ theorem yoneda{CC: category}(F: functor (op CC) SortCat)(X: CC.Ob):
     rw [H1 f]
     simp
   . apply F.id
+
+def yoneda_embedding(CC: category):
+  functor CC (FunctorCat (op CC) SortCat)
+:= {
+  onOb := homFun
+  onHom {C D} h := {
+    η C := CC.compose h
+    naturality := by
+      simp [SortCat, op, homFun]
+      intro B A f
+      funext g
+      apply CC.assoc
+  }
+  id := by
+    simp [FunctorCat, SortCat, homFun, natTransId, op]
+    intro A
+    funext B f
+    simp
+  compose := by
+    simp [FunctorCat, SortCat, homFun, natTransComp, op]
+    intro B C D h g
+    funext A f
+    rw [CC.assoc]
+}
+
+theorem yoneda_fully_faithful(CC: category):
+  fullyFaithful (yoneda_embedding CC)
+:= by
+  split_ands
+  . intro X Y nt
+    use nt.η X (CC.id X)
+    simp [yoneda_embedding]
+    congr
+    funext Z f
+    have H1 := @nt.naturality X Z f
+    simp [FunctorCat, yoneda_embedding, homFun, op, SortCat] at H1
+    let g := λ x ↦ CC.compose (nt.η X x) f
+    change _ = g at H1
+    trans  g (CC.id X)
+    rfl
+    rw [←H1]
+    simp
+  . intro X Y f1 f2 H1
+    let ye := yoneda_embedding CC
+    let nt₁ := (ye.onHom f1)
+    let nt₂ := (ye.onHom f2)
+    change nt₁ = nt₂ at H1
+    have H2: nt₁.η X (CC.id X) = nt₂.η X (CC.id X) := by rw [H1]
+    simp [nt₁, nt₂, ye, yoneda_embedding] at H2
+    assumption
