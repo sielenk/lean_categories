48395376311757532d5a1616fa5a8552724bff33
Marvin Sielenkemper <sielenk@gmx.de>
2025-12-30 14:49:53 +0100

    refactored sortCatEqualizer

--- Primus/SortCat.lean
+++ Primus/SortCat.lean
@@ -130,70 +130,34 @@ by
   tauto
 
 
-def sortCatEqualizer{X Y: SortCat.Ob}(f₁ f₂: SortCat.Hom X Y): equalizer f₁ f₂ := by
-  let JJ := equalizerDiagram
-  let CC := SortCat
-  change CC.Ob at X Y
-  change CC.Hom X Y at f₁ f₂
-  let F: functor JJ CC := equalizerFunctor f₁ f₂
-  let DD := coneCat F
-  let E: CC.Ob := { x // f₁ x = f₂ x }
-  let e: CC.Hom E X := Subtype.val
-  let π i: CC.Hom E (F.onOb i) :=
-    match i with
-    | equalizerOb.A => e
-    | equalizerOb.B => CC.compose f₁ e
-  let H{J₁ J₂}(f : JJ.Hom J₁ J₂): CC.compose (F.onHom f) (π J₁) = π J₂ :=
-    match f with
-    | equalizerHom.idA => CC.left_id _
-    | equalizerHom.idB => CC.left_id _
-    | equalizerHom.f₁ => rfl
-    | equalizerHom.f₂ => funext λ ⟨ee, H⟩ ↦ Eq.symm H
-  let T: DD.Ob := coneOb.mk E π H
-  let foo(T': DD.Ob): CC.Hom T'.N E := by
-    let ⟨E', π', H'⟩ := T'
-    let e':  CC.Hom E' X := π' equalizerOb.A
-    let e'f: CC.Hom E' Y := π' equalizerOb.B
-    change CC.Hom E' E
-    intro ee'
-    apply Subtype.mk (e' ee')
-    let H₁: (λee ↦ f₁ (e' ee)) = e'f := @H' _ _ equalizerHom.f₁
-    let H₂: (λee ↦ f₂ (e' ee)) = e'f := @H' _ _ equalizerHom.f₂
-    trans e'f ee'
-    · rw [←H₁]
-    · rw [←H₂]
-
-  let hom(T': DD.Ob): DD.Hom T' T := by
-    apply coneHom.mk (foo T')
-    let ⟨N', π', H'⟩ := T'
-    intro jj
-    simp [foo, CC, SortCat]
-    ext x
-    simp [T, π, e]
-    cases jj
-    · simp
-    · simp [CC, SortCat]
-      rw [←H' equalizerHom.f₁]
-      simp [CC, SortCat, F]
-      rfl
-
-  let unique X: ∀g, g = hom X := by
-    let ⟨Nx, πx, Hx⟩ := X
-    simp [hom, foo]
-    intro ⟨g, Hg⟩
-    simp [CC, SortCat, T, E] at g Hg
-    congr
-    simp [CC, SortCat, T, E]
-    ext x
-    have H2 : πx equalizerOb.A x = π equalizerOb.A (g x) := by
-      rw [←Hg equalizerOb.A]
-    congr!
-    rw [H2]
-
-  exact {
-    T := T
-    hom := hom
-    unique := unique
+def sortCatEqualizer{X Y: SortCat.Ob}(f₁ f₂: SortCat.Hom X Y): equalizer f₁ f₂ :=
+  {
+    T := {
+      N:= { x // f₁ x = f₂ x }
+      π J X := match J with
+        | equalizerOb.A => X.val
+        | equalizerOb.B => f₁ X.val
+      comm f := match f with
+        | equalizerHom.idA => SortCat.left_id _
+        | equalizerHom.idB => SortCat.left_id _
+        | equalizerHom.f₁ => rfl
+        | equalizerHom.f₂ => funext (λ N => Eq.symm N.property)
+    }
+    hom X := {
+      h x := ⟨
+        X.π equalizerOb.A x,
+         Eq.trans
+          (congrArg (λ h => h x) (X.comm equalizerHom.f₁))
+          (Eq.symm
+            (congrArg (λ h => h x) (X.comm equalizerHom.f₂))
+          )
+      ⟩
+      fac J := match J with
+        | equalizerOb.A => rfl
+        | equalizerOb.B => X.comm equalizerHom.f₁
+    }
+    unique _ g :=
+      coneHom.ext _ (funext (λ_ => Subtype.ext (congr_fun (g.fac equalizerOb.A) _)))
   }
 
 def sortCatPullback{X₁ X₂ Y: SortCat.Ob}
@@ -225,17 +189,14 @@ by
       fac J := match J with
         | pullbackOb.A₁ => rfl
         | pullbackOb.A₂ => rfl
-        | pullbackOb.B =>
-          funext (λ x => congrArg (λ h => h x) (X.comm pullbackHom.f₁))
+        | pullbackOb.B => X.comm pullbackHom.f₁
     }
     unique X g := ?unique
   }
   · case unique =>
-    trans ⟨g.h, g.fac⟩
-    · rfl
-    · congr
-      funext x
-      apply Subtype.ext
-      apply Prod.ext
-      · apply congrArg (λ h => h x) (g.fac pullbackOb.A₁)
-      · apply congrArg (λ h => h x) (g.fac pullbackOb.A₂)
+    apply coneHom.ext
+    funext x
+    apply Subtype.ext
+    apply Prod.ext
+    · apply congrArg (λ h => h x) (g.fac pullbackOb.A₁)
+    · apply congrArg (λ h => h x) (g.fac pullbackOb.A₂)
